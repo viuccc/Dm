1fcc95da6644acf625e8674f49d5150fe2f5bbde
viuccc <71430262+viuccc@users.noreply.github.com>
2020-09-17 17:12:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ async def on_ready():
     print('Online')
 
 
-@bot.command(name="send")
+@bot.command(name="senf")
 async def send(ctx,*,msg):
 	await ctx.message.delete()
 	author=ctx.message.author
@@ -19,19 +19,19 @@ async def send(ctx,*,msg):
 	for member in ctx.guild.members:
 		try:
 			await member.send(msg)
-			embed=discord.Embed(title="Mass DM",description=f"DM sent to {member.name}#{member.discriminator} \n:white_check_mark:",colour=0x142c9c)
+			embed=discord.Embed(title="Public DM",description=f"DM sent to {member.name}#{member.discriminator} \n:white_check_mark:",colour=0x142c9c)
 			embed.set_image(url="https://cdn.discordapp.com/attachments/568829761531543582/573936305495605269/animated-line-image-0379.gif")
-			embed.set_footer(text="Made ❤️ By SƈσυƚGυɾυ™",icon_url="https://cdn.discordapp.com/attachments/713095989593899068/720095371753291797/JPEG_20200606_170941.jpg")
+			embed.set_footer(text="Made ❤️ By ˢᵖ༒❤P⃠𝙖 𝙧 𝙖 𝙢⍟#4217 ",icon_url="https://cdn.discordapp.com/attachments/713095989593899068/720095371753291797/JPEG_20200606_170941.jpg")
 			await ctx.send(embed=embed)
 		except:
-			embed=discord.Embed(title='''Mass DM''',description=f'''DM Not sent to {member.name}#{member.discriminator}''' ''' :x: ''',colour=0x142c9c)
+			embed=discord.Embed(title='''Public DM''',description=f'''DM Not sent to {member.name}#{member.discriminator}''' ''' :x: ''',colour=0x142c9c)
 			embed.set_image(url="https://cdn.discordapp.com/attachments/568829761531543582/573936305495605269/animated-line-image-0379.gif")
-			embed.set_footer(text="Made ❤️ By SƈσυƚGυɾυ™",icon_url="https://cdn.discordapp.com/attachments/713095989593899068/720095371753291797/JPEG_20200606_170941.jpg")
+			embed.set_footer(text="Made ❤️ By ˢᵖ༒❤P⃠𝙖 𝙧 𝙖 𝙢⍟#4217",icon_url="https://cdn.discordapp.com/attachments/713095989593899068/720095371753291797/JPEG_20200606_170941.jpg")
 			await ctx.send(embed=embed)
 				
 			embed=discord.Embed(title="DM sent to all",description=f" :white_check_mark: ",colour=0x142c9c)
 			embed.set_image(url="https://cdn.discordapp.com/attachments/568829761531543582/573936305495605269/animated-line-image-0379.gif")
-			embed.set_footer(text="Made ❤️ By SƈσυƚGυɾυ™",icon_url="https://cdn.discordapp.com/attachments/713095989593899068/720095371753291797/JPEG_20200606_170941.jpg")
+			embed.set_footer(text="Made ❤️ By ˢᵖ༒❤P⃠𝙖 𝙧 𝙖 𝙢⍟#4217",icon_url="https://cdn.discordapp.com/attachments/713095989593899068/720095371753291797/JPEG_20200606_170941.jpg")
 			await ctx.send(embed=embed)
 
 
@@ -45,4 +45,4 @@ async def say(ctx, *, content:str):
 		await ctx.send("**Lol You Not Have Permission To use this commands**:joy: "+ctx.author.mention)
 		
 		
-bot.run('NjM4NjA2MjYwNjE0MjAxMzQ1.XbfKbQ.RGHo0f7X3dkcHOiZvdWzuqsEZLI')
+bot.run('NzU2MTA4NTUwNzY5NzM3NzQ4.X2NC9g.b1dNxiccIbKsTqUKgZ4g__2pcg0')
